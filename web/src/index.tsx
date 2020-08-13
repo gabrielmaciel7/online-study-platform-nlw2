f5bf7ce48daa5246cbd6d7ad77fb69e4c9d01097
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-08-13 14:25:08 -0300

    config react environment variables

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,11 @@
+import * as dotenv from "dotenv";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
