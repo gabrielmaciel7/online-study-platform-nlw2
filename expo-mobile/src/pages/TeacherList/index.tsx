c208ee51a931d1a238ca46d3b3a73c32fa9d4ebd
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-08-20 16:21:41 -0300

    add mobile filter teacher list

--- expo-mobile/src/pages/TeacherList/index.tsx
+++ expo-mobile/src/pages/TeacherList/index.tsx
@@ -1,15 +1,69 @@
-import React from "react";
-import { View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import styles from "./styles";
 
 import PageHeader from "../../Components/PageHeader";
 import TeacherItem from "../../Components/TeacherItem";
+import {
+  TextInput,
+  BorderlessButton,
+  RectButton,
+} from "react-native-gesture-handler";
 
 const TeacherList: React.FC = () => {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Qual a matéria?"
+              placeholderTextColor="#c1bccc"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o dia?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o horário?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+            </View>
+
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
