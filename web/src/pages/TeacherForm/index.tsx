7fad8e1334bbfe9849f6a1ee7d76d597ffdb8626
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-09-07 10:16:41 -0300

    add register animation

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,19 +1,22 @@
-import React, { useState, FormEvent } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, FormEvent } from "react";
+import { useHistory } from "react-router-dom";
+import Lottie from "react-lottie";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Textarea from '../../components/Textarea';
-import Select from '../../components/Select';
+import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
-import warningIcon from '../../assets/images/icons/warning.svg';
+import warningIcon from "../../assets/images/icons/warning.svg";
+import okAnimation from "../../assets/animation/ok.json";
+import errorAnimation from "../../assets/animation/error.json";
 
-import './styles.css';
+import "./styles.css";
 
 const TeacherForm = () => {
-  const history = useHistory()
+  const history = useHistory();
 
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
@@ -24,54 +27,138 @@ const TeacherForm = () => {
   const [cost, setCost] = useState("");
 
   const [scheduleItems, setScheduleItems] = useState([
-    { week_day: 0, from: '', to: '' }
+    { week_day: 0, from: "", to: "" },
   ]);
 
+  const [animation, setAnimation] = useState({
+    isVisible: false,
+    isStopped: false,
+    isPaused: false,
+    animationOk: true,
+    animationError: false,
+    animationLabel: "",
+  });
+
+  const defaultOptionsAnimation = {
+    loop: false,
+    autoplay: true,
+    animationData: animation.animationOk ? okAnimation : errorAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   function addNewScheduleItem() {
-    setScheduleItems([
-      ...scheduleItems,
-      { week_day: 0, from: "", to: "" }
-    ])
+    setScheduleItems([...scheduleItems, { week_day: 0, from: "", to: "" }]);
   }
 
-  function setScheduleItemValue(position: number, field: string, value: string) {
+  function setScheduleItemValue(
+    position: number,
+    field: string,
+    value: string
+  ) {
     const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
       if (index === position) {
-        return { ...scheduleItem, [field]: value }
+        return { ...scheduleItem, [field]: value };
       }
 
       return scheduleItem;
-    })
+    });
 
-    setScheduleItems(updatedScheduleItems)
+    setScheduleItems(updatedScheduleItems);
   }
 
-  function handleCreateClass(e: FormEvent) {
+  async function handleCreateClass(e: FormEvent) {
     e.preventDefault();
 
-    api.post('classes', {
-      name, avatar, whatsapp, bio, subject, cost: Number(cost), schedule: scheduleItems
-    }).then(() => {
-      alert('Cadastro realizado com sucesso"');
+    try {
+      await api.post("classes", {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost: Number(cost),
+        schedule: scheduleItems,
+      });
 
-      history.push('/');
-    }).catch(() => {
-      alert('Erro no cadastro.');
-    })
+      setAnimation({
+        ...animation,
+        isVisible: true,
+        animationOk: true,
+        animationError: false,
+        animationLabel: "Cadastro realizado com sucesso",
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 2500));
+
+      setAnimation({
+        ...animation,
+        isVisible: false,
+      });
+
+      history.push("/");
+    } catch (error) {
+      setAnimation({
+        ...animation,
+        isVisible: true,
+        animationOk: false,
+        animationError: true,
+        animationLabel: "Erro no cadastro.",
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 2500));
+
+      setAnimation({
+        ...animation,
+        isVisible: false,
+      });
+    }
   }
 
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!" description="O primeiro passo é preencher esse formulário de inscrição" />
+      <PageHeader
+        title="Que incrível que você quer dar aulas!"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
 
       <main>
         <form onSubmit={handleCreateClass}>
           <fieldset>
             <legend>Seus dados</legend>
-            <Input name="name" label="Nome completo" value={name} onChange={(e) => { setName(e.target.value) }} />
-            <Input name="avatar" label="Avatar" value={avatar} onChange={(e) => { setAvatar(e.target.value) }} />
-            <Input name="whatsapp" label="WhatsApp" value={whatsapp} onChange={(e) => { setWhatsapp(e.target.value) }} />
-            <Textarea name="bio" label="biografia" value={bio} onChange={(e) => { setBio(e.target.value) }} />
+            <Input
+              name="name"
+              label="Nome completo"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <Input
+              name="avatar"
+              label="Avatar"
+              value={avatar}
+              onChange={(e) => {
+                setAvatar(e.target.value);
+              }}
+            />
+            <Input
+              name="whatsapp"
+              label="WhatsApp"
+              value={whatsapp}
+              onChange={(e) => {
+                setWhatsapp(e.target.value);
+              }}
+            />
+            <Textarea
+              name="bio"
+              label="biografia"
+              value={bio}
+              onChange={(e) => {
+                setBio(e.target.value);
+              }}
+            />
           </fieldset>
 
           <fieldset>
@@ -81,26 +168,37 @@ const TeacherForm = () => {
               name="subject"
               label="Matéria"
               value={subject}
-              onChange={(e) => { setSubject(e.target.value) }}
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
               options={[
-                { value: 'Artes', label: 'Artes' },
-                { value: 'Matemática', label: 'Matemática' },
-                { value: 'Biologia', label: 'Biologia' },
-                { value: 'Física', label: 'Física' },
-                { value: 'Química', label: 'Química' },
-                { value: 'História', label: 'História' },
-                { value: 'Português', label: 'Português' },
-                { value: 'Educação Física', label: 'Educação Física' },
+                { value: "Artes", label: "Artes" },
+                { value: "Matemática", label: "Matemática" },
+                { value: "Biologia", label: "Biologia" },
+                { value: "Física", label: "Física" },
+                { value: "Química", label: "Química" },
+                { value: "História", label: "História" },
+                { value: "Português", label: "Português" },
+                { value: "Educação Física", label: "Educação Física" },
               ]}
             />
 
-            <Input name="cost" label="Custo da sua hora por aula" value={cost} onChange={(e) => { setCost(e.target.value) }} />
+            <Input
+              name="cost"
+              label="Custo da sua hora por aula"
+              value={cost}
+              onChange={(e) => {
+                setCost(e.target.value);
+              }}
+            />
           </fieldset>
 
           <fieldset>
             <legend>
               Horários disponíveis
-            <button type="button" onClick={addNewScheduleItem}>+ Novo horário</button>
+              <button type="button" onClick={addNewScheduleItem}>
+                + Novo horário
+              </button>
             </legend>
 
             {scheduleItems.map((scheduleItem, index) => {
@@ -110,15 +208,17 @@ const TeacherForm = () => {
                     name="week_day"
                     label="Dia da semana"
                     value={scheduleItem.week_day}
-                    onChange={(e) => setScheduleItemValue(index, 'week_day', e.target.value)}
+                    onChange={(e) =>
+                      setScheduleItemValue(index, "week_day", e.target.value)
+                    }
                     options={[
-                      { value: '0', label: 'Domingo' },
-                      { value: '1', label: 'Segunda-feira' },
-                      { value: '2', label: 'Terça-feira' },
-                      { value: '3', label: 'Quarta-feira' },
-                      { value: '4', label: 'Quinta-feira' },
-                      { value: '5', label: 'Sexta-feira' },
-                      { value: '6', label: 'Sábado' },
+                      { value: "0", label: "Domingo" },
+                      { value: "1", label: "Segunda-feira" },
+                      { value: "2", label: "Terça-feira" },
+                      { value: "3", label: "Quarta-feira" },
+                      { value: "4", label: "Quinta-feira" },
+                      { value: "5", label: "Sexta-feira" },
+                      { value: "6", label: "Sábado" },
                     ]}
                   />
 
@@ -127,7 +227,9 @@ const TeacherForm = () => {
                     label="Das"
                     type="time"
                     value={scheduleItem.from}
-                    onChange={(e) => setScheduleItemValue(index, 'from', e.target.value)}
+                    onChange={(e) =>
+                      setScheduleItemValue(index, "from", e.target.value)
+                    }
                   />
 
                   <Input
@@ -135,26 +237,42 @@ const TeacherForm = () => {
                     label="Até"
                     type="time"
                     value={scheduleItem.to}
-                    onChange={(e) => setScheduleItemValue(index, 'to', e.target.value)}
+                    onChange={(e) =>
+                      setScheduleItemValue(index, "to", e.target.value)
+                    }
                   />
                 </div>
-              )
+              );
             })}
-
           </fieldset>
 
           <footer>
             <p>
               <img src={warningIcon} alt="Aviso importante" />
-            Importante! <br />
-            Preencha todos os dados
-          </p>
+              Importante! <br />
+              Preencha todos os dados
+            </p>
             <button type="submit">Salvar cadastro</button>
           </footer>
         </form>
       </main>
+
+      {animation.isVisible ? (
+        <div className="modal">
+          <div className="animation">
+            <Lottie
+              options={defaultOptionsAnimation}
+              height={400}
+              width={400}
+              isStopped={animation.isStopped}
+              isPaused={animation.isPaused}
+            />
+            <h1>{animation.animationLabel}</h1>
+          </div>
+        </div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
 export default TeacherForm;
